b62aac0de801db875b0dcdd4f9fde4e03f3dae34
Mirck, S (ITCDCC) - KLM <stefan.mirck@klm.com>
2015-11-03 18:25:40 +0100

    Refactored ribbon.

---
+++ assets/sytac-io-ribbon.js
@@ -0,0 +1,16 @@
+;(function () {
+    'use strict';
+
+    var el = document.querySelector('body > .page-content'),
+        ribbon = document.querySelector('.sytac-io-ribbon-wrapper'),
+        elTop = Math.floor(el.getBoundingClientRect().top) + document.body.scrollTop;
+
+    document.addEventListener('scroll', function (e) {
+        console.log(document.body.scrollTop, elTop);
+        if (document.body.scrollTop > elTop) {
+            !ribbon.className.match(/\sinverse/) && (ribbon.className += ' inverse');
+        } else {
+            ribbon.className = ribbon.className.replace(/\sinverse/, '');
+        }
+    });
+}());
